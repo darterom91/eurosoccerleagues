9b031c1a743cc2722a9acc67ce794b418ba02ad5
David Artero <d.arterom@gmail.com>
2020-05-06 23:38:29 +0200

    add in route the teamPL page and add in other pages props TeamPL[id] and modify app.vue

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,20 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path: '/premierLeague',
+    name: 'PremierLeague',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "PremierLeague" */ '../views/PremierLeague.vue')
+  },
+  {
+    path: '/teamPL/:id',
+    name: 'TeamPL',
+    props: true,
+    component: () => import(/* webpackChunkName: "TeamPL" */ '../views/TeamPL.vue')
+  },
+  {
     path: '/team/:id',
     name: 'Team',
     props: true,
