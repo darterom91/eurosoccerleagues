8592b9d1e2aafaf0ce879ad3ec269ee974314360
David Artero <d.arterom@gmail.com>
2020-05-17 00:59:17 +0200

    add components matches, menu and standing & add page Ligue1.vue and TeamL1.vue Ligue 1

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
     path: '/about',
@@ -16,15 +16,26 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/ligue1',
+    name: 'Ligue1',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "ligue1" */ '../views/Ligue1.vue'),
   },
   {
     path: '/team/:id',
     name: 'Team',
     props: true,
-    component: () => import(/* webpackChunkName: "Teams" */ '../views/Team.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "Teams" */ '../views/Team.vue'),
+  },
+];
 
 const router = new VueRouter({
   routes
---
+++ src/views/Ligue1.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <cpHeader/>
+    <l1Menu/>
+    <l1Matches/>
+    <l1Standings/>
+    <cpFooter/>
+  </div>
+</template>
+
+<script>
+import cpHeader from '@/components/CP_Header.vue'
+import l1Menu from '@/components/Ligue1/L1_menu.vue'
+import l1Matches from '@/components/Ligue1/L1_matches.vue'
+import l1Standings from '@/components/Ligue1/L1_standings.vue'
+import cpFooter from '@/components/CP_Footer.vue'
+export default {
+  components:{
+    cpHeader,
+    l1Menu,
+    l1Matches,
+    l1Standings,
+    cpFooter
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
