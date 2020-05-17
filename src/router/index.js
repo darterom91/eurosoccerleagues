bcaee8a5d1b5f356673e94644f7432cdc3ee49d1
David Artero <d.arterom@gmail.com>
2020-05-17 02:29:30 +0200

    add components matches, menu and standing & add page Ligue1.vue and TeamL1.vue Ligue 1

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path: '/ligueone',
+    name: 'LigueOne',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "ligueone" */ '../views/LigueOne.vue')
+  },
+  {
     path: '/team/:id',
     name: 'Team',
     props: true,
